c4f9bd4ab1689e22132810f7b2f2e7bdf126ab33
nurdin442d <nurdinsuryana0@gmail.com>
2022-11-25 14:10:46 +0700

    Add Menu 'Biodata'

--- LeaveManagement.WEB7/Controllers/HomeController.cs
+++ LeaveManagement.WEB7/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace LeaveManagement.WEB7.Controllers
             return View();
         }
 
+        public IActionResult Biodata()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
