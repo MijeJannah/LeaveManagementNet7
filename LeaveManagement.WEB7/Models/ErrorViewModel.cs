4a11a9689005bef7c7f8d144924401229312a8fb
nurdin442d <nurdinsuryana0@gmail.com>
2022-11-25 11:10:27 +0700

    Add project files.

---
+++ LeaveManagement.WEB7/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LeaveManagement.WEB7.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
